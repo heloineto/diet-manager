b9278bed3f6fcc0aac84e2ab376fdaaf8e9a6b02
Heloi Neto <hey@heloineto.com>
2021-08-13 12:41:15 -0300

    updated AuthIllustration

--- components/decoration/AuthIllustration/AuthIllustration.tsx
+++ components/decoration/AuthIllustration/AuthIllustration.tsx
@@ -1,20 +1,18 @@
 import styles from './AuthIllustration.module.css';
 import classNames from 'classnames';
 
-import Image from 'next/image';
-
 const AuthIllustration = () => {
   return (
     <div
       className={classNames(
-        styles['auth-illustration']
-        // 'relative left-0 top-0 col-span-full lg:col-span-5 xl:col-span-6 flex justify-center'
+        styles['illustration'],
+        'relative left-0 top-0 w-full lg:w-1/2 flex justify-center'
       )}
     >
       <img
         className={classNames(
-          styles['auth-wordmark']
-          //
+          styles['wordmark'],
+          'absolute lg:top-0 lg:left-0 mt-8 h-12'
         )}
         src="/wordmark.svg"
         alt="wordmark"
