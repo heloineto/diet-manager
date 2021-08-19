81bbd849b58e06a60b63a55e5c226f4dcf6f56c7
Heloi Neto <hey@heloineto.com>
2021-08-19 09:10:31 -0300

    update before major changes

--- components/navigation/NavBar/NavBar.tsx
+++ components/navigation/NavBar/NavBar.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 import DietManagerWordmark from '@components/decoration/DietManagerWordmark';
 import DietManagerLogo from '@components/decoration/DietManagerLogo';
 import { UserContext } from '@lib/context';
-import { Button } from '@material-ui/core';
+import { Button, Hidden, makeStyles } from '@material-ui/core';
 import { LogoutIcon } from '@heroicons/react/outline';
 import { leave } from '@lib/auth';
 
@@ -20,42 +20,45 @@ const NavBar = ({ className, current }: Props) => {
   const { userDetails } = useContext(UserContext);
 
   return (
-    <div
-      className={clsx(
-        className,
-        'w-1/12 lg:w-3/12 flex flex-col flex-1 h-screen'
-      )}
-    >
-      <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-        <div className="flex items-center flex-shrink-0 justify-start px-2 h-10 gap-x-2">
+    <div className={clsx(className, 'flex flex-col flex-1 h-screen')}>
+      <div className="flex flex-col py-5 overflow-y-auto">
+        <div className="flex items-center justify-start flex-shrink-0 h-10 gap-x-2">
           <DietManagerLogo />
           <DietManagerWordmark className="hidden lg:block" />
         </div>
-        <nav className="mt-5 flex-1 space-y-1 pr-2" aria-label="sidebar">
+        <nav className="mt-5 space-y-1 pr-2" aria-label="sidebar">
           {items.map(({ name, label, href, Icon }) => {
             const isCurrent = current === name;
 
             return (
               <Link key={name} href={href}>
-                <div
-                  className={clsx(
-                    isCurrent
-                      ? 'bg-primary-200 text-gray-900 hover:text-gray-900 hover:bg-primary-300'
-                      : 'text-gray-600 hover:text-gray-900 hover:bg-gray-200',
-                    'group flex items-center px-2 py-2 text-sm lg:text-base font-semibold rounded-md'
-                  )}
-                >
-                  <Icon
+                <Hidden mdDown>
+                  <Button
                     className={clsx(
                       isCurrent
-                        ? 'text-gray-500'
-                        : 'text-gray-400 group-hover:text-gray-500',
-                      'mr-3 flex-shrink-0 h-6 w-6 lg:h-8 lg:w-8'
+                        ? 'bg-primary-200 text-primary-800 hover:text-primary-900 hover:bg-primary-300'
+                        : 'text-gray-600 hover:text-gray-800 hover:bg-gray-200',
+                      'group text-base font-bold w-full'
                     )}
-                    aria-hidden="true"
-                  />
-                  <span className="flex-1">{label}</span>
-                </div>
+                    classes={{
+                      label: 'static flex justify-start',
+                      startIcon: 'static',
+                    }}
+                    startIcon={
+                      <Icon
+                        className={clsx(
+                          isCurrent
+                            ? 'text-primary-800'
+                            : 'text-gray-600 group-hover:text-gray-800',
+                          'mr-3 flex-shrink-0 h-6 w-6 lg:h-8 lg:w-8'
+                        )}
+                        aria-hidden="true"
+                      />
+                    }
+                  >
+                    {label}
+                  </Button>
+                </Hidden>
               </Link>
             );
           })}
@@ -66,7 +69,7 @@ const NavBar = ({ className, current }: Props) => {
           className="w-full"
           variant="outlined"
           color="secondary"
-          startIcon={<LogoutIcon />}
+          startIcon={<LogoutIcon className="h-7 w-7" />}
           onClick={leave}
         >
           Sair
