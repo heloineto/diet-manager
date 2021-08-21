5f3048ccaa0c5c1c7b182116843ed92539b0375a
heloi-neto <hey@heloineto.com>
2021-08-21 17:43:44 -0300

    fixed bugs

--- components/decoration/AuthIllustration/AuthIllustration.tsx
+++ components/decoration/AuthIllustration/AuthIllustration.tsx
@@ -1,23 +1,19 @@
-import styles from './AuthIllustration.module.css';
 import clsx from 'clsx';
+import DietManagerWordmark from '../DietManagerWordmark';
 
 const AuthIllustration = () => {
   return (
     <div
-      className={clsx(
-        styles['illustration'],
-        'relative left-0 top-0 w-full lg:w-1/2 flex justify-center'
-      )}
+      className={clsx(`
+        relative left-0 top-0 w-full lg:w-1/2 flex justify-center min-h-[32vw]
+
+        before:h-[125vw] before:w-[125vw] before:z-[-1] before:bg-primary-200 before:absolute
+        before:rounded-full before:top-0 before:left-1/2 before:transform before:-translate-x-1/2 before:-translate-y-3/4
+        
+        lg:before:fixed lg:before:top-1/2 lg:before:left-0 lg:before:-translate-x-1/2 lg:before:-translate-y-1/2
+        `)}
     >
-      <img
-        className={clsx(
-          styles['wordmark'],
-          'absolute lg:top-0 lg:left-0 mt-8 h-12'
-        )}
-        src="/wordmark.svg"
-        alt="wordmark"
-        // layout="fill"
-      />
+      <DietManagerWordmark className="h-14 w-56 absolute lg:top-0 lg:left-0 mt-8 fill-[#047857]" />
     </div>
   );
 };
