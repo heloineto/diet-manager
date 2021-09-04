72be2d8be6e2d1c01f9af15f9944f1414c5c4f5f
Heloi Neto <hey@heloineto.com>
2021-09-04 20:39:46 -0300

    updated

--- components/forms/profile/UpdateNutritionGoals/UpdateNutritionGoals.tsx
+++ components/forms/profile/UpdateNutritionGoals/UpdateNutritionGoals.tsx
@@ -20,7 +20,7 @@ const UpdateNutritionGoals = ({ className, onClose }: Props) => {
   // 'goals.nutrition.fat',
   // 'goals.nutrition.kcal',
 
-  const { carbInfo, protInfo, fatInfo } = useMacrosInfo();
+  const { carbInfo, protInfo, fatInfo, kcalInfo } = useMacrosInfo();
 
   const updateNutritionGoals = () => {};
 
@@ -57,7 +57,7 @@ const UpdateNutritionGoals = ({ className, onClose }: Props) => {
       >
         {({ handleSubmit, submitting, values }) => (
           <form className={clsx(className)} onSubmit={handleSubmit}>
-            <div className="grid grid-flow-col grid-rows-2 grid-cols-1 gap-y-6 sm:grid-cols-3 sm:gap-x-3">
+            <div className="grid grid-flow-col grid-rows-2 grid-cols-1 gap-y-5 sm:grid-cols-3 sm:gap-x-3">
               {[carbInfo, protInfo, fatInfo].map(({ label, key }) => (
                 <>
                   <TextField
@@ -66,6 +66,10 @@ const UpdateNutritionGoals = ({ className, onClose }: Props) => {
                     name={key}
                     type="number"
                     autoComplete="off"
+                    disabled={inputMode !== 'grams'}
+                    onClick={() => {
+                      if (inputMode !== 'grams') setInputMode('grams');
+                    }}
                     fieldProps={{
                       parse: (value) => {
                         if (value < 0) return 0;
@@ -84,9 +88,14 @@ const UpdateNutritionGoals = ({ className, onClose }: Props) => {
                   <TextField
                     key={key}
                     label={label}
-                    name={`${key}2`}
+                    name={`${key}`}
                     type="number"
                     autoComplete="off"
+                    disabled={inputMode !== 'percentage'}
+                    onClick={() => {
+                      if (inputMode !== 'percentage')
+                        setInputMode('percentage');
+                    }}
                     fieldProps={{
                       parse: (value) => {
                         if (value > 100) return 100;
@@ -108,6 +117,38 @@ const UpdateNutritionGoals = ({ className, onClose }: Props) => {
                   />
                 </>
               ))}
+            </div>
+
+            <div className="flex justify-center">
+              <TextField
+                className="my-5 w-1/2"
+                key={kcalInfo.key}
+                label={kcalInfo.label}
+                name={kcalInfo.key}
+                type="number"
+                autoComplete="off"
+                onClick={() => {
+                  if (inputMode !== 'percentage') setInputMode('percentage');
+                }}
+                fieldProps={{
+                  parse: (value) => {
+                    if (value > 100) return 100;
+                    if (value < 0) return 0;
+                    return value;
+                  },
+                }}
+                InputProps={{
+                  inputProps: {
+                    min: 0,
+                    max: 100,
+                  },
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <div className="font-bold text-gray-500">%</div>
+                    </InputAdornment>
+                  ),
+                }}
+              />
             </div>
 
             <div className="sm:col-span-6">
