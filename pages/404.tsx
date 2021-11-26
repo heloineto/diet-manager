58e179ea7389daf794cbfdb84c5e8c0dfdf20201
Heloi Neto <hey@heloineto.com>
2021-11-25 22:12:04 -0300

    fixed eslint errors

--- pages/404.tsx
+++ pages/404.tsx
@@ -7,7 +7,7 @@ const _404: NextPage = () => {
     <div className="min-h-screen pt-16 pb-12 flex flex-col bg-white">
       <main className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex-shrink-0 flex justify-center">
-          <Link href="/">
+          <Link href="/" passHref>
             <IconButton className="h-16 w-16 p-0 inline-flex">
               <DietManagerLogo className="h-16 w-16" />
             </IconButton>
